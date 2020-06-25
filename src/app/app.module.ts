4f26f072be108eb9bea92c1d29e16aa3ea6ab121
Assis, Raphael A <raphael.a.assis@accenture.com>
2020-06-25 20:38:35 -0300

    criacao do frontend da aplicacao web api em c#

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CadastroFornecedorComponent } from './cadastro-fornecedor/cadastro-fornecedor.component';
+import { ConsultaFornecedorComponent } from './consulta-fornecedor/consulta-fornecedor.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ConsultaProdutoComponent } from './consulta-produto/consulta-produto.component';
+
+//importando a biblioteca de rotas do angular
+import { RouterModule, Routes } from '@angular/router';
+
+//importando a bilbioteca para realizar as requisições HTTP para a API
+import { HttpClientModule } from '@angular/common/http';
+
+//importando as bibliotecas para desenvolver formularios dinamicos
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//registrando uma rota para cada componente
+const appRoutes : Routes = [
+  { path : 'cadastro-fornecedor', component : CadastroFornecedorComponent },
+  { path : 'consulta-fornecedor', component : ConsultaFornecedorComponent },
+  { path : 'cadastro-produto', component : CadastroProdutoComponent },
+  { path : 'consulta-produto', component : ConsultaProdutoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroFornecedorComponent,
+    ConsultaFornecedorComponent,
+    CadastroProdutoComponent,
+    ConsultaProdutoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cadastro-fornecedor/cadastro-fornecedor.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-cadastro-fornecedor',
+  templateUrl: './cadastro-fornecedor.component.html',
+  styleUrls: ['./cadastro-fornecedor.component.css']
+})
+export class CadastroFornecedorComponent implements OnInit {
+
+  //atributos
+  mensagemProcessamento:string;
+  mensagemSucesso:string;
+  mensagemErro:string;
+
+  errosNome:[];
+  errosCnpj:[];
+
+  apiUrl = "http://localhost:58799/api/fornecedores"
+
+  //método construtor
+  constructor(private httpClient:HttpClient) { }
+
+  //método
+  ngOnInit(): void {
+  }
+
+  //método
+  cadastrarFornecedor(formCadastro){
+    
+    //exibindo um loading na página
+    this.mensagemProcessamento = "Processando, por favor aguarde.";
+    this.mensagemSucesso = "";
+    this.mensagemErro = "";
+    this.errosNome = [];
+    this.errosCnpj = [];
+
+    this.httpClient.post(this.apiUrl,formCadastro.value,
+      {responseType: 'text'})
+      .subscribe( //retorno da api
+        (result)=>{ // pegando a resposta de sucesso da api
+          this.mensagemSucesso = result;
+          this.mensagemProcessamento = "";
+      },
+      (e)=>{ // Pegando o erro da api
+        const response = JSON.parse(e.error);
+
+        if(e.status == 400){// erro de validacao
+            this.errosNome = response.errors.Nome;
+            this.errosCnpj = response.errors.Cnpj;
+        }else if(e.status == 500) { //Erro de servidor
+          this.mensagemErro = response.errors;
+        }
+      }
+    );
+  }
+
+}
